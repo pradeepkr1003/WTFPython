654c039225aab30da168daa540b3cd48667f3e3c
Pradeep Kumar <33798093+pradeepkr1003@users.noreply.github.com>
2022-05-22 13:32:13 +0530

    Update items_sum_with_recursion.py

--- items_sum_with_recursion.py
+++ items_sum_with_recursion.py
@@ -3,6 +3,8 @@ Two Steps:
     1. Figure out the base case
     2. Divide the problem until it becomes the base case.
 """
+import timeit
+from gettext import find
 from typing import List
 
 
@@ -14,20 +16,80 @@ def sum_list(list_: List[int], list_sum_: int = 0) -> int:
     return list_sum_
 
 
-
 # [Not Working]
 def _sum_list(list_: List[int], list_sum_: int = 0) -> int:
     # sum of list with divide and concure method
     if list_:
         list_sum_ += list_.pop()
-        _sum_list(list_, list_sum_)
-        # return _sum_list(list_, list_sum_)
+        # _sum_list(list_, list_sum_)
+        return _sum_list(list_, list_sum_)
     return list_sum_
 
 
+# > -- [Find the Max Value in a list]
+
+def find_max(_list_: List[int], max_: int = 0) -> int:
+    val = _list_.pop()
+
+    if _list_:
+        if val > max_:
+            max_ = val
+
+        find_max(_list_, max_)
+
+    else:
+        print('the max value is', max_)
+        return max_
+
+
+def qsort(_list_: List[int]) -> int:
+    # Quick Sort
+    if _list_.__len__() < 2:
+        return _list_
+
+    else:
+        pivot = _list_[0]
+
+        smaller = [x for x in _list_[1:] if x <= pivot]
+        greater = [x for x in _list_[1:] if x >= pivot]
+
+        return qsort(smaller) + [pivot] + qsort(greater)
+
+
+def qsort_faster(_list_: List[int]) -> int:
+    # Quick Sort a bit faster
+    if _list_.__len__() < 2:
+        return _list_
+
+    else:
+        len_ = len(_list_) // 2
+        pivot = _list_[len_]
+
+        smaller = [x for x in _list_[1:] if x <= pivot]
+        greater = [x for x in _list_[1:] if x >= pivot]
+
+        return qsort(smaller) + [pivot] + qsort(greater)
+
+
 # StdIn:
-_num_list = [1, 2]
+# ======
+# _num_list = [1,2,3,4,5,6,23,7,8,9,10]
+_num_list = [x for x in range(100)]
 
 # Stdout
-print('stdout: [from first function] ', sum_list(_num_list))
-print('stdout: [from second function] ', _sum_list(_num_list))
+# ======
+
+# print('stdout: [from first function] ', sum_list(_num_list))
+# print('stdout: [from second function] ', _sum_list(_num_list))
+
+# -
+# print('MAX (func) :', find_max(_num_list))
+# -
+
+
+print('qsort with middle pivot', timeit.repeat(
+    'qsort_faster([x for x in range(500)])', 'from __main__ import qsort_faster', number=1))
+# - 
+print('qsort with starting value as pivot', timeit.repeat(
+    'qsort([x for x in range(950)])', 'from __main__ import qsort', number=1))
+# qsort(_num_list)
